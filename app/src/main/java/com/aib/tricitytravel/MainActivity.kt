e6ec497f027f5574e001af4902c8c97ce938dca6
Maciej Królik <maciejkrolik@outlook.com>
2019-04-06 20:28:35 +0200

    Add settings screen

--- app/src/main/java/com/aib/tricitytravel/MainActivity.kt
+++ app/src/main/java/com/aib/tricitytravel/MainActivity.kt
@@ -2,11 +2,26 @@ package com.aib.tricitytravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/aib/tricitytravel/ui/MainFragment.kt
+++ app/src/main/java/com/aib/tricitytravel/ui/MainFragment.kt
@@ -1,15 +1,34 @@
 package com.aib.tricitytravel.ui
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.aib.tricitytravel.R
 
 class MainFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                Navigation.findNavController(view!!).navigate(R.id.action_mainFragment_to_settingsFragment)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
